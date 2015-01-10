8f5180477d2d13e65936da9103d98c6588eb4af7
kenwilcox <wilcoxk@gmail.com>
2015-01-09 22:01:46 -0700

    Stop the music when we terminate - or we crash

--- Space Cannon/KXAppDelegate.m
+++ Space Cannon/KXAppDelegate.m
@@ -49,6 +49,8 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  SKView *view = (SKView*)self.window.rootViewController.view;
+  ((KXMyScene*)view.scene).appExiting = YES;
 }
 
 @end
