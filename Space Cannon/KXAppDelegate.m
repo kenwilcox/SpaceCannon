d94ac4df4407d84000cc9580d9feb626c629fb61
kenwilcox <wilcoxk@gmail.com>
2015-01-10 23:26:54 -0700

    Trying to track down a crash

--- Space Cannon/KXAppDelegate.m
+++ Space Cannon/KXAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import <SpriteKit/SpriteKit.h>
+#import <AVFoundation/AVFoundation.h>
 #import "KXAppDelegate.h"
 #import "KXMyScene.h"
 
@@ -22,6 +23,10 @@
 {
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
   // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  
+  // prevent audio crash
+  [[AVAudioSession sharedInstance] setActive:NO error:nil];
+  
   SKView *view = (SKView*)self.window.rootViewController.view;
   ((KXMyScene*)view.scene).appExiting = YES;
 }
@@ -30,6 +35,10 @@
 {
   // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  
+  // prevent audio crash
+  [[AVAudioSession sharedInstance] setActive:NO error:nil];
+  
   SKView *view = (SKView*)self.window.rootViewController.view;
   ((KXMyScene*)view.scene).appExiting = YES;
 }
@@ -49,6 +58,10 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  
+  // prevent audio crash
+  [[AVAudioSession sharedInstance] setActive:NO error:nil];
+  
   SKView *view = (SKView*)self.window.rootViewController.view;
   ((KXMyScene*)view.scene).appExiting = YES;
 }
